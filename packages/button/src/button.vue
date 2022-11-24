bb8146310c821b794621d1d3873e7f5adc54fabb
fmh <648558925@qq.com>
2022-11-25 00:24:12 +0800

    monorepo project packages

---
+++ packages/button/src/button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="click">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LgButton',
+  inheritAttrs: false,
+  methods: {
+    click(evt) {
+      this.$emit('click', evt)
+      evt.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
