6a7a49f0c3e001fa059b4f3b4e24e7b027428ffc
fmh <648558925@qq.com>
2023-01-01 23:44:04 +0800

    packages

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import FmForm from './src/Form-test.vue'
 
 Vue.use(ElementUI)
-
-new Vue({
-  el: '#app',
-  render: h => h(FmForm)
-})
